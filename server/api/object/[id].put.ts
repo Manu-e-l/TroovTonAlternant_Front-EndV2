fb2097f02b650eb8a824c1fd0e9207a79a010352
Manuel Elhadide <manuel.elhadide@gmail.com>
2022-11-15 00:35:05 +0100

    Ajout Put Object & modif comment sur Create.post

--- server/api/object/[id].put.ts
+++ server/api/object/[id].put.ts
@@ -2,11 +2,13 @@ import ObjectModel from "~~/models/Object.model";
 import { ObjectSchema } from "~~/server/validation";
 
 export default defineEventHandler(async (event) => {
-    // Obtenir data depuis  body
+    // Obtenir data depuis body
     const body = await useBody(event);
 
+    const id = event.context.params.id;
+
     // validité  (erreur renvoyé )
-    const { error } = ObjectSchema.validate(body);
+    const { error } = ObjectSchema.validate(body, { abortEarly: true, allowUnknown: true });
     if (error) {
         throw createError({
             message: error.message.replace(/"/g, ""),
@@ -15,10 +17,10 @@ export default defineEventHandler(async (event) => {
         });
     }
 
-    // objet crée 
+    // objet modifié 
     try {
-        await ObjectModel.create(body);
-        return { message: "Objet crée" };
+        await ObjectModel.findByIdAndUpdate(id, body);
+        return { message: "Objet modifié" };
     } catch (e) {
         throw createError({
             message: e.message,
