026d0f404f55104b6242202edf768cd2efcd199e
Manuel Elhadide <manuel.elhadide@gmail.com>
2022-11-15 01:24:33 +0100

    ajout @type/bcrypt & User Model et Validation

--- server/validation.ts
+++ server/validation.ts
@@ -1,8 +1,23 @@
 import Joi from "joi";
 
 export const ObjectSchema = Joi.object({
-    objectName: Joi.string().trim(true).required().min(2),
+    objectName: Joi.string()
+        .trim()
+        .required()
+        .min(2),
     userWhoFound: Joi.array(),
-    foundDate: Joi.date().required(),
-    pageCount: Joi.number(),
+    foundDate: Joi.date()
+        .required(),
+    pageCount: Joi.number()
+});
+
+export const UserSchema = Joi.object({
+    email: Joi.string()
+        .required()
+        .lowercase()
+        .trim(),
+    password: Joi.string()
+        .required()
+        .max(133)
+        .min(6)
 });
